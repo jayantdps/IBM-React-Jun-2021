b7f0c9bee9df500f001d3d106542e06e7699d6c7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-02 17:32:19 +0530

    Routing implementation

---
+++ bug-tracker-app/src/Home.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Magna officia id et amet veniam ad duis non aliqua enim est dolor. Minim id enim aliquip ea consequat nulla voluptate consectetur. Tempor veniam et veniam et pariatur culpa eiusmod consequat labore anim incididunt ipsum. Aliquip aliqua ad minim id voluptate officia proident voluptate cupidatat est sit elit.Officia consectetur amet cillum proident incididunt incididunt dolor enim ipsum mollit. Tempor ad minim do consequat nisi occaecat mollit deserunt eiusmod qui mollit. Est ipsum ullamco ea aute ut quis sunt esse adipisicing commodo. Ea deserunt dolor do sit ut minim anim. Excepteur dolor consequat ipsum non. Qui mollit amet quis tempor fugiat.</p>
+      <p>Ea aliqua in est do non qui fugiat tempor consectetur. Aliqua do velit anim ex dolor reprehenderit sit amet consequat minim. Et do ea fugiat et culpa Lorem ex non adipisicing pariatur.Ipsum eiusmod do ea fugiat duis officia sint in velit occaecat ex exercitation. Lorem ad occaecat consequat cillum voluptate quis pariatur amet duis duis sit. Nisi nostrud incididunt enim pariatur ullamco reprehenderit sint aliqua elit occaecat. Excepteur eu excepteur occaecat non eu culpa commodo.</p>
+      <p>Voluptate culpa qui Lorem non sunt ea ea cillum minim officia pariatur ullamco. Et fugiat esse laborum minim in. Mollit minim sint ad incididunt fugiat id. Laboris officia nisi excepteur Lorem laboris. Enim nisi consectetur eiusmod sint aliqua et eu esse Lorem qui consequat aliqua nisi id.Cillum Lorem quis magna qui irure aute commodo velit dolor proident eiusmod. Pariatur et exercitation nulla dolor in ut. Qui amet anim proident cillum.</p>
+    </div>
+  );
+}
+
+export default App;
